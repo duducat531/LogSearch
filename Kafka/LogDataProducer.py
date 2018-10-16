22b758c21dc1262029959fb74839bc347e9d3950
duducat531 <wangyining531@gmail.com>
2018-10-16 14:53:59 +0800

    update submodule

--- Kafka/LogDataProducer.py
+++ Kafka/LogDataProducer.py
@@ -1,7 +1,7 @@
 # MetricCollector
 from KafkaInstance.DataProducer import DataProducer
 if __name__ == '__main__':
-    logDataConsumer = DataProducer(topicName='ebay_log', serverAddr='localhost:9092', groupId='test_log_data_producer')
+    logDataConsumer = DataProducer(topicName='ebay_log', serverAddr='localhost:9092')
     logData = {'corr_id': 'ejsQ5ufDFCmyHG6v',
                'url_api': 'sample/login',
                'node_id': 'NSflDxKmr2AR',
