6a23c745d84a51394b7cb563838a189b6b3c9096
duducat531 <wangyining531@gmail.com>
2018-10-16 14:49:04 +0800

    rename topic name

--- Kafka/LogDataConsumer.py
+++ Kafka/LogDataConsumer.py
@@ -1,7 +1,7 @@
 from KafkaInstance.DataConsumer import DataConsumer
 from DBInstance.DBInstance import DBInstance
 if __name__ == '__main__':
-    logDataConsumer = DataConsumer(topicName='ebay_metric', serverAddr='localhost:9092',
+    logDataConsumer = DataConsumer(topicName='ebay_log', serverAddr='localhost:9092',
                                    groupId='test_log_data_consumer',
                                    dbInstance=DBInstance("localhost", "wyn", "123456", "ebay_log"))
     logDataConsumer.ReceiveAndSaveToDB()
